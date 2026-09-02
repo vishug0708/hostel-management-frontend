701fb37b54e9b787ba126a3a89ddb9ae2cfdc55c
Vishu Gupta <vishug0708@gmail.com>
2026-09-02 18:46:02 +0530

    Update RectorLogin.jsx

--- src/pages/rector/RectorLogin.jsx
+++ src/pages/rector/RectorLogin.jsx
@@ -36,7 +36,7 @@ function RectorLogin() {
             setError("");
 
             const response = await fetch(
-                "http://localhost:5000/api/rector/login",
+                `${import.meta.env.VITE_API_URL}/api/rector/login`,
                 {
                     method: "POST",
                     headers: {
